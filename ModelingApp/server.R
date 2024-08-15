ba5ad025178bd3d1e8642f3074fc192065287519
Sam O'Ferrell <saoferre@ncsu.edu>
2024-08-15 13:45:15 -0400

    started app - looking good so far allowed user to select columns for model

--- ModelingApp/server.R
+++ ModelingApp/server.R
@@ -8,21 +8,71 @@
 #
 
 library(shiny)
+library(DT)
 
 # Define server logic required to draw a histogram
 function(input, output, session) {
+#function for modeling
+  modeling_function <- function(method, variables){
+    variable_list <- paste(variables, collapse = " + ")
+    func_model <- as.formula(paste("diagnosis ~", variable_list))
+    if (method == "kNN"){
+      kNNFit_func <- train(func_model, 
+                           data = train,
+                           method = "knn",
+                           preProcess = c("center", "scale"),
+                           trControl = trainControl(method = "repeatedcv", 
+                                                    number = 10,
+                                                    repeats = 3),
+                           tuneGrid = expand.grid(k = seq(from = 1, to = 40, by = 1)))
+      return(kNNFit_func)
+    }
+    if (method == "tree"){
+      treeFit_func <- train(func_model, 
+                            data = train,
+                            method = "rpart",
+                            preProcess = c("center", "scale"),
+                            trControl = trainControl(method = "repeatedcv", 
+                                                     number = 10,
+                                                     repeats = 3),
+                            tuneGrid = expand.grid(cp = seq(from = 0, to = 0.1, by = 0.001)))    
+      return(treeFit_func)
+    }
+    if (method == "logistic"){
+      logFit_func <- train(func_model, 
+                           data = train,
+                           preProcess = c("center", "scale"),
+                           method = "glm",
+                           family = "binomial",
+                           trControl = trainControl(method = "repeatedcv", 
+                                                    number = 10,
+                                                    repeats = 3))
+      return(logFit_func)
+    }
+  }
 
-    output$distPlot <- renderPlot({
+# reading in data 
+  data <- read_csv("../breast-cancer.csv") |>
+    rename("concave_points_mean" = "concave points_mean",
+           "concave_points_se" = "concave points_se",
+           "concave_points_worst" = "concave points_worst")
+  set.seed(426)
+  # Creating an 80/20 split
+  split <- createDataPartition(y = data$diagnosis, p = 0.8, list = FALSE)
+  train <- data[split, ]
+  test <- data[-split, ]
+  
+  output$summary <- DT::renderDataTable({
 
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-
+      output$columns <- renderUI({
+        checkboxGroupInput("variables", "Select Predictor Variables", 
+                           choices = colnames(data))})
+      data
     })
+  output$formula <- renderPrint({
+    selected_variables <- input$variables
+    fit <- modeling_function(method = "logistic", variables = selected_variables)
+    print(fit)
+  })
 
 }
--- ModelingApp/ui.R
+++ ModelingApp/ui.R
@@ -13,21 +13,19 @@ library(shiny)
 fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Breast Cancer Modeling App"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+          uiOutput("columns")
         ),
-
+        
         # Show a plot of the generated distribution
         mainPanel(
-            plotOutput("distPlot")
+          dataTableOutput("summary"),
+          verbatimTextOutput("formula")
+          
         )
     )
 )
